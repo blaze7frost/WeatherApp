fc637e450d41889ccadd3252095ae72e0fd6e4a4
blaze7frost <girinaveen128@gmail.com>
2022-08-01 00:00:40 +0530

    first commit

---
+++ config.js
@@ -0,0 +1,8 @@
+const constants = {
+    openWeatherMap: {
+        BASE_URL: "https://api.openweathermap.org/data/2.5/weather?q=",
+        SECRET_KEY: "f0f2bdc34d2266f4289bdddeadaf6c83"
+    }
+}
+
+module.exports = constants;
